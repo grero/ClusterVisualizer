6c69efcf3df741cbda735cbb0195f8268137278b
Grogee <NUS@Grogee-2.local>
2010-10-05 13:03:18 +0800

    Fixed the problem. I had forgotten to add glClear(GL_DEPTH_BUFFER_BIT) to the drawRect function

--- FeatureView.m
+++ FeatureView.m
@@ -532,6 +532,7 @@ static void drawAnObject()
     //}
     glClearColor(0,0,0,0);
     glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_DEPTH_BUFFER_BIT);
     if(dataloaded)
     {
         drawAnObject();
