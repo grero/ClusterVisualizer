ecf4e84b706e777cc9b1f59c71cd496f1c07df44
Roger Herikstad <roger.herikstad@gmail.com>
2012-07-09 16:50:51 +0800

    Added function to compute eigenvalue decomposition of a square symmetric matrix

--- utils.c
+++ utils.c
@@ -60,4 +60,44 @@ double chi2_cdf(double x, int df)
     return d;
 }
 
+void eigen(double *A, unsigned int n, double **S, double **V)
+{
+	//use gsl routines to compute eigenvalue decomposition of the square, symmetric matrix A
+	//allocates space for the output S and V; S is a vector containing the sorted eigenvalues and V is 2D-vector containing the corresponding eigenvectors. Note that the eigenvectors are in the rows of V
+	//from example located at http://www.gnu.org/software/gsl/manual/html_node/Eigenvalue-and-Eigenvector-Examples.html
+	unsigned int i,j;
+	gsl_matrix_view m;
+	gsl_vector *eval;
+    gsl_vector_view v;
+	gsl_matrix *evec;
+	gsl_eigen_symmv_workspace *w;
+	
+	//allocate space for output
+	//eigenvalues
+	*S = malloc(n*sizeof(double));
+	//eigenvectors
+	*V = malloc(n*n*sizeof(double));
+
+	m = gsl_matrix_view_array(A,n,n);
+	eval = gsl_vector_alloc(n);
+	evec = gsl_matrix_alloc(n,n);
+	w = gsl_eigen_symmv_alloc(n);
+	gsl_eigen_symmv(&m.matrix,eval,evec,w);
+	gsl_eigen_symmv_free(w);
+	gsl_eigen_symmv_sort(eval,evec,GSL_EIGEN_SORT_ABS_ASC);
+	//copy to output
+	for(i=0;i<n;i++)
+	{
+		(*S)[i] = gsl_vector_get(eval,i);
+		for(j=0;j<n;j++)
+		{
+			(*V)[i*n+j] = gsl_matrix_get(evec,j,i);
+		}
+
+	}
+	gsl_vector_free(eval);
+	gsl_matrix_free(evec);
+
+}
+
 
--- utils.h
+++ utils.h
@@ -1,5 +1,9 @@
 #include <gsl/gsl_specfunc.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_eigen.h>
 #include <math.h>
+
 unsigned int *histogram_sorted(float *data, int datal, float*bins, int binsl,unsigned int *counts);
 int matrix_inverse(double *A,int N,double *det,int *sign);
 double chi2_cdf(double x, int df);
+void eigen(double *A, unsigned int n, double **S, double **V);
